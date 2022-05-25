7ca8603739f366094cce319512e0271a1643146a
edggmym@gmail.com <edggmym@gmail.com>
2022-05-25 17:16:37 -0400

    update

--- models/Usuario.php
+++ models/Usuario.php
@@ -40,6 +40,7 @@ class Usuario extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
                 [['usua_apellidos'], 'string', 'max' => 150],
                 [['usua_ci'], 'string', 'min' => 3 ,'max' => 15],
                 [['usua_password','usua_cuenta',], 'string', 'min' => 5 ,'max' => 50],
+                [['usua_ci', 'usua_cuenta'], 'unique', 'targetAttribute' => ['usua_ci', 'usua_cuenta'], 'message' => 'El Carnet o la Cuenta del usuario ya esta registrado. verifique por favor'],    
                 [['usua_rol'], 'string', 'max' => 50],
         ];
     }
