c1536442f13e6d99d5b1c9b48f0705f7d379072b
bautistadave <bautistadave007@gmail.com>
2022-08-05 19:07:46 -0400

    anulaciones

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    //'dsn' => 'pgsql:host=181.177.143.185;dbname=db_urkupina',
-    'dsn' => 'pgsql:host=181.177.143.185;dbname=urkupinia',
+    'dsn' => 'pgsql:host=181.177.143.185;dbname=db_urkupina',
+    //'dsn' => 'pgsql:host=181.177.143.185;dbname=urkupinia',
     //'dsn' => 'pgsql:host=localhost;dbname=urkupina',
     'username' => 'gamq',
     'password' => 'gamq.22',
