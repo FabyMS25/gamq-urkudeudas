af9ea3bf2531c3d26329161c49a07fab49e7b4de
faby <moralessfaby.dev@gmail.com>
2023-08-07 10:41:11 -0400

    fixed observaciones y sentajes

--- views/generador/_columns.php
+++ views/generador/_columns.php
@@ -41,6 +41,11 @@ return [
         'filter' =>false
     ],
     [
+        'class'=>'\kartik\grid\DataColumn',
+        'attribute'=>'detalle_nro_limite',
+        'filter' =>false
+    ],
+    [
          'class'=>'\kartik\grid\DataColumn',
          'attribute'=>'detalle_cantidad',
          'filter' =>false
