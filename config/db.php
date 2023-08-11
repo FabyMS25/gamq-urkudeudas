c829b740cec00ed8ce563647f684740c6ab5edc5
devgamq <devgamq@gmail.com>
2023-08-11 08:03:48 -0400

    fix detalles servidor

--- config/db.php
+++ config/db.php
@@ -6,9 +6,9 @@ return [
     //'dsn' => 'pgsql:host=localhost;dbname=sys_urkupina',
     
     'class' =>   'yii\db\Connection',
-    'dsn'   =>   'pgsql:host=181.177.143.180;dbname=db_urkupina',
+    'dsn'   =>   'pgsql:host=localhost;dbname=db_urkupina',
     // 'dsn'   =>   'pgsql:host=181.177.143.185;dbname=test_urku',
     'username' => 'postgres',
-    'password' => 'postgres.23',
+    'password' => 'devgamq',
     'charset' => 'utf8',
 ];
