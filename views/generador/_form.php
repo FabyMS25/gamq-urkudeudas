037636b3990157db0f416f08d785f977c4c955d5
Andres Castro M <castroandresrm@gmail.com>
2023-08-18 15:09:28 -0400

    Fix: onchange  en el formulario de sentajes

--- views/generador/_form.php
+++ views/generador/_form.php
@@ -1,93 +1,105 @@
 <?php
+
+use yii\helpers\Html;
+use app\models\Descargos;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
-use app\models\Descargos;
-use yii\helpers\Html;
 
 $listaResponsablesDesc = (new Descargos())->listaResponsables();
 $items = ArrayHelper::map($listaResponsablesDesc, 'desc_id', 'desc_responsable');
-//var_dump($listaResponsablesDesc);
 ?>
 
 <div class="descargos-form">
-
     <?php $form = ActiveForm::begin(); ?>
-
-        <?= $form->field($model, 'desc_id')->dropDownList($items, ['prompt'=>'** Seleccione una opcion **']) ?>
-
-        <div class="row">
-            <div class="col-md-6">
-                <?= $form->field($model, 'detalle_precio')->textInput(['id' => 'detalle_precio']) ?>
-            </div>
-            <div class="col-md-6">
-                <?= $form->field($model, 'detalle_nro_inicio')->textInput(['id' => 'detalle_nro_inicio']) ?>
-            </div>
+    <?= $form->field($model, 'desc_id')->dropDownList(
+        $items,
+        ['prompt' => '** Seleccione una opcion **']
+    ) ?>
+    <div class="row">
+        <div class="col-md-6">
+            <?= $form->field($model, 'detalle_precio')->textInput([
+                'id' => 'detalle_precio',
+                'type' => 'number',
+                'min' => 0,
+                'onchange' => 'onchangeValues()'
+            ]) ?>
         </div>
-        
-        <div class="row">
-            <div class="col-md-6">
-                <?= $form->field($model, 'detalle_nro_limite')->textInput(['id' => 'detalle_nro_limite', 'onchange'=>'actualizar()']) ?>
-            </div>
-            <div class="col-md-6">
-                <?= $form->field($model, 'detalle_cantidad_anulado')->textInput(['id' => 'detalle_cantidad_anulado', 'onchange'=>'actualizar()']) ?>
-            </div>
+        <div class="col-md-6">
+            <?= $form->field($model, 'detalle_nro_inicio')->textInput([
+                'id' => 'detalle_nro_inicio',
+                'type' => 'number',
+                'min' => 0,
+                'onchange' => 'onchangeValues()'
+            ]) ?>
         </div>
+    </div>
 
-        <div class="row">
-            <div class="col-md-6">
-                <?= 
-                    $form->field($model, 'detalle_cantidad')->textInput([
-                        'readonly' => true,
-                        'type'    =>'number',
-                        'id' => 'detalle_cantidad' 
-                    ])        
-                ?>
-            </div>
-            <div class="col-md-6">
-                <?= $form->field($model, 'detalle_importe_bs')->textInput(['readonly' => true, 'id'=>'detalle_importe_bs']) ?> 
-            </div>
-            <div class="col-md-6">
-            </div>
-        </div>  
-  
-	<?php if (!Yii::$app->request->isAjax){ ?>
-	  	<div class="form-group">
-	        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-	    </div>
-	<?php } ?>
+    <div class="row">
+        <div class="col-md-6">
+            <?= $form->field($model, 'detalle_nro_limite')->textInput([
+                'id' => 'detalle_nro_limite',
+                'type' => 'number',
+                'min' => 0,
+                'onchange' => 'onchangeValues()'
+            ]) ?>
+        </div>
+        <div class="col-md-6">
+            <?= $form->field($model, 'detalle_cantidad_anulado')->textInput([
+                'id' => 'detalle_cantidad_anulado',
+                'type' => 'number',
+                'min' => 0,
+                'onchange' => 'onchangeValues()'
+            ]) ?>
+        </div>
+    </div>
 
-    <?php ActiveForm::end();?>
-    
+    <div class="row">
+        <div class="col-md-6">
+            <?= $form->field($model, 'detalle_cantidad')->textInput([
+                'readonly' => true,
+                'type' => 'number',
+                'id' => 'detalle_cantidad'
+            ])
+            ?>
+        </div>
+        <div class="col-md-6">
+            <?= $form->field($model, 'detalle_importe_bs')->textInput([
+                'readonly' => true,
+                'type' => 'number',
+                'id' => 'detalle_importe_bs'
+            ]) ?>
+        </div>
+        <div class="col-md-6">
+        </div>
+    </div>
+
+    <?php if (!Yii::$app->request->isAjax) { ?>
+        <div class="form-group">
+            <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        </div>
+    <?php } ?>
+
+    <?php ActiveForm::end(); ?>
+
 </div>
 
 
 <script type="text/javascript">
+    function onchangeValues() {
+        var precio = document.getElementById("detalle_precio").value;
+        var inicio = document.getElementById("detalle_nro_inicio").value;
+        var limite = document.getElementById("detalle_nro_limite").value;
+        var anulado = document.getElementById("detalle_cantidad_anulado").value;
 
-function actualizar() {           
-  var precio  = document.getElementById("detalle_precio").value;
-  var inicio  = document.getElementById("detalle_nro_inicio").value;
-  var limite  = document.getElementById("detalle_nro_limite").value;
-  var anulado  = document.getElementById("detalle_cantidad_anulado").value;
-  
-  if(inicio > 0 && limite > 0) { 
-    var cantidad = (parseFloat(limite) - parseFloat(inicio)) + 1;
-    var totalCantidad = (parseFloat(cantidad) - parseFloat(anulado));
-    var totalImporte  = parseFloat(precio) * parseFloat(totalCantidad);
-
-    //if (limite <= inicio ) {
-      //  alert('NRO LIMITE NO PUEDE SER MAYOR A NRO INICIO');
-        //document.getElementById("detalle_nro_inicio").setAttribute('value', '');
-        //document.getElementById("detalle_nro_limite").setAttribute('value', ''); 
-    //} else {
-        document.getElementById("detalle_cantidad").setAttribute('value', totalCantidad);
-        document.getElementById("detalle_importe_bs").setAttribute('value', totalImporte);
-    //}
-
-  }else {
-        document.getElementById("detalle_cantidad").setAttribute('value', '');
-        document.getElementById("detalle_importe_bs").setAttribute('value', '');  
-  }
-
-}
-  
+        if (precio >= 0 && inicio > 0 && limite > 0 && anulado >= 0) {
+            var cantidad = (parseFloat(limite) - parseFloat(inicio)) + 1;
+            var totalCantidad = (parseFloat(cantidad) - parseFloat(anulado));
+            var totalImporte = parseFloat(precio) * parseFloat(totalCantidad);
+            document.getElementById("detalle_cantidad").setAttribute('value', totalCantidad);
+            document.getElementById("detalle_importe_bs").setAttribute('value', totalImporte);
+        } else {
+            document.getElementById("detalle_cantidad").setAttribute('value', null);
+            document.getElementById("detalle_importe_bs").setAttribute('value', null);
+        }
+    }
 </script>
