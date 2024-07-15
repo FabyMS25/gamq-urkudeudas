07beaf103b6827c619928ffac7043e1a3700a9ea
Andres Castro M <castroandresrm@gmail.com>
2024-07-15 16:39:11 -0400

    Fix: Clasifier code

--- controllers/PagosController.php
+++ controllers/PagosController.php
@@ -216,7 +216,7 @@ class PagosController extends Controller
     public function actionPreliquidar($id)
     {
         $mensaje = '';
-        $codigoClasificador = '29156';
+        $codigoClasificador = '22976';
         $resultado = false;
         $this->verificarSesion();
         $request = Yii::$app->request;
