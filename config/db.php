3231a4e57ac3470ac9474237658980ac91e6d543
Fabiola Morales <moralessfaby.dev@gmail.com>
2026-06-21 19:38:45 -0400

    feat: updated to github

--- config/db.php
+++ config/db.php
@@ -3,9 +3,9 @@
 
 return [
     'class' =>   'yii\db\Connection',
-//     'dsn' => 'pgsql:host=localhost;port=5433;dbname=db_urku',
-    'dsn'   =>   'pgsql:host=181.177.143.187;port=5434;dbname=db_urku2026',
+    'dsn' => 'pgsql:host=localhost;port=5433;dbname=db_urku',
+//     'dsn'   =>   'pgsql:host=181.177.143.187;port=5434;dbname=db_urku2026',
     'username' => 'postgres',
-    'password' => 'Admin*PG18',
+    'password' => 'ADMIN',
     'charset' => 'utf8',
 ];
