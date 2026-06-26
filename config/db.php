a014bd20f6b8e9dfffd8c96cf1074e161897301b
Fabiola Morales <moralessfaby.dev@gmail.com>
2026-06-26 18:04:07 -0400

    updated boleta

--- config/db.php
+++ config/db.php
@@ -3,10 +3,10 @@
 
 return [
     'class' =>   'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5433;dbname=db_urku',
-//     'dsn'   =>   'pgsql:host=localhost;port=5434;dbname=db_urku2026',
+//     'dsn' => 'pgsql:host=localhost;port=5433;dbname=db_urku',
+    'dsn'   =>   'pgsql:host=localhost;port=5434;dbname=db_urku2026',
     'username' => 'postgres',
-    'password' => 'ADMIN',
-//     'password' => 'Admin*PG18',
+//     'password' => 'ADMIN',
+    'password' => 'Admin*PG18',
     'charset' => 'utf8',
 ];
